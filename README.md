143f79df372e4a2d861543f4d8d0eb181b7b3d26
Krasimir <krasimir.st.tsonev@gmail.com>
2026-01-15 09:33:37 +0200

    Initial and probably the only commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pills-tracker
-Static vanilla HTML, CSS and JavaScript app for tracking your daily doze of pills
+
+Static vanilla HTML, CSS and JavaScript app for tracking your daily dose of pills
+
+I got prescribed with 5 different pills to take every day, and I wanted a simple app to track my daily intake. The app stores data in the browser's local storage, so no backend is needed. There is no server or anything. Just load the `index.html` file in your browser and start using it.
---
+++ logic.js
@@ -0,0 +1,72 @@
+const START_DATE = new Date("2026-01-14");
+const TODAY = new Date();
+
+const medicines = [
+  { name: "Еманера", times: ["08:00", "-", "-"], desc: "1х1 / 30 min преди храна" },
+  { name: "Дуспаталин", times: ["07:30", "-", "20:00"], desc: "2х1 / преди храна" },
+  { name: "Креон", times: ["07:30", "13:00", "20:00"], desc: "3х1 / по време на храна" },
+  { name: "Хулудексан", times: ["8:00", "13:30", "20:30"], desc: "3х1 / след храна" },
+  { name: "Бутирин", times: ["-", "-", "20:30"], desc: "1х1 / след храна" }
+];
+
+window.addEventListener("load", () => {
+  const $main = $("main");
+  const days = getDaysForOneMonth(START_DATE);
+  const tables = [];
+
+  days.forEach((day) => {
+    const isInThePast = day.getDate() < TODAY.getDate() && day.getMonth() === TODAY.getMonth();
+    let table = [`<div class='table ${isInThePast ? "past" : ""}'>`];
+    table.push(
+      `<div class='date'>${day.getDate().toString().padStart(2, "0")}.${(day.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}</div>`
+    );
+    table.push("<div class='medicines'>");
+    medicines.forEach((medicine) => {
+      table.push("<div class='medicine'>");
+      table.push(`<div class="medicine-name">
+        ${medicine.name}<br />
+        <span class="medicine-desc">${medicine.desc}</span>
+      </div>`);
+      medicine.times.forEach((time) => {
+        const key = `${medicine.name}_${day.toISOString()}_${time}`;
+        const isTaken = localStorage.getItem(key) === "taken";
+        table.push(
+          `<div class='time-slot ${isTaken ? "taken" : ""}' onclick="onTimeSlotClick(this, '${key}')">${time}</div>`
+        );
+      });
+      table.push("</div>");
+    });
+    table.push("</div>");
+    table.push("</div>");
+    tables.push(table.join(""));
+  });
+  $main.innerHTML = tables.join("");
+});
+
+function onTimeSlotClick(element, key) {
+  const data = localStorage.getItem(key);
+  if (!data || data !== "taken") {
+    element.classList.add("taken");
+    localStorage.setItem(key, "taken");
+  } else {
+    localStorage.setItem(key, "nah");
+    element.classList.remove("taken");
+  }
+}
+
+// ... utilities
+function $(selector) {
+  return document.querySelector(selector);
+}
+function getDaysForOneMonth(startDate) {
+  const date = new Date(startDate);
+  const days = [];
+  let i = 0;
+  while (i++ < 31) {
+    days.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  return days;
+}
